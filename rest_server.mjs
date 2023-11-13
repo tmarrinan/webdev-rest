93c6cd10a1044f09507afa8a845460d6f37aa6a0
Catherine Kieffer <kief4528@stthomas.edu>
2023-11-13 15:40:01 -0600

    can show initial codes, modified the DbSelect to work for that

--- rest_server.mjs
+++ rest_server.mjs
@@ -25,19 +25,37 @@ let db = new sqlite3.Database(db_filename, sqlite3.OPEN_READWRITE, (err) => {
     }
 });
 
-// Create Promise for SQLite3 database SELECT query 
+// // Create Promise for SQLite3 database SELECT query 
+// function dbSelect(query, params) {
+//     return new Promise((resolve, reject) => {
+//         db.all(query, params, (err, rows) => {
+//             if (err) {
+//                 reject(err);
+//             }
+//             else {
+//                 resolve(rows);
+//             }
+//         });
+//     });
+// }
+
+// this funciton is from the server.mjs we wrote in dynamiccereal - this works, the above function does not work for me
 function dbSelect(query, params) {
-    return new Promise((resolve, reject) => {
+    let p = new Promise((resolve, reject) => {
+        //inside here we put our asynchronous calls
+        //we 
         db.all(query, params, (err, rows) => {
             if (err) {
-                reject(err);
+                reject(err); //this will trigger the .catch block
             }
             else {
                 resolve(rows);
             }
-        });
+        })  //then the parameters that substitue for our question marks
     });
+    return p;
 }
+
 
 // Create Promise for SQLite3 database INSERT or DELETE query
 function dbRun(query, params) {
@@ -59,8 +77,33 @@ function dbRun(query, params) {
 // GET request handler for crime codes
 app.get('/codes', (req, res) => {
     console.log(req.query); // query object (key-value pairs after the ? in the url)
+
+    let sql = 'SELECT * FROM Incidents';
+    let params = [];
+    // let statement = " WHERE";
+
+    // if (req.query.hasOwnProperty("mfr")) {
+    //     console.log(req.query["mfr"]);
+    //     let sql2 = statement + " mfr=?";
+    //     sql = sql+sql2;
+    //     params.push(req.query["mfr"].toUpperCase())
+    //     statement = " AND";
+    // };
+    console.log(sql)
+    console.log(params)
+
+    dbSelect(sql, params)
+    .then((rows) => {
+        res.status(200).type('json').send(rows); //we just send the rows back as JSON
+
+    })
+    .catch((error) => {
+        res.status(500).type('txt').send(error); //send back whatever the error is 
+    });
+
+
     
-    res.status(200).type('json').send({}); // <-- you will need to change this
+    // res.status(200).type('json').send({}); // <-- you will need to change this
 });
 
 // GET request handler for neighborhoods
