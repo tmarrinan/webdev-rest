101e1f5adffe7db090182930becfe7f0022ebf96
inotp <tran6854@stthomas.edu>
2023-12-13 01:56:09 -0600

    readded SearchT page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue'
 import Search from '../views/Search.vue'
 import Upload from '../views/Upload.vue'
+import SearchT from '../views/Search\ copy.vue'
 
 // 1. Define route components.
 // 2. Define some routes
@@ -11,6 +12,7 @@ const routes = [
   { path: '/', name: "Home", component: Home },
   { path: '/search', name: "Search", component: Search },
   { path: '/upload', name: "Upload", component: Upload },
+  { path: '/searchT', name: "SearchT", component: SearchT },
 ]
 
 // 3. Create the router instance and pass the `routes` option
