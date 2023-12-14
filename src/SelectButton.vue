0d870c93928a21144d46233676b4b1dca28bc619
Catherine Kieffer <kief4528@stthomas.edu>
2023-12-14 17:45:59 -0600

    added Noah's changes, trying to get initializeCrimes to run each time you move the page, but not working still

---
+++ src/SelectButton.vue
@@ -0,0 +1,46 @@
+<!-- SelectButton.vue -->
+<template>
+    <button class="button" type="button" @click="toggleSelectCrime">
+      {{ buttonText }}
+    </button>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      address: String, // Assuming 'address' is passed as a prop
+      date: String,
+      time: String,
+      incident: String,
+      //delete: Function,
+      case_number: String,
+      onSelect: Function, // Function to be called when crime is selected
+      onUnselect: Function, // Function to be called when crime is unselected
+    },
+    data() {
+      return {
+        selected: false,
+      };
+    },
+    methods: {
+      toggleSelectCrime() {
+        if (!this.selected) {
+          // Select logic
+          // You can pass the address or any other necessary data to the parent component
+          this.onSelect(this.address, this.date, this.time, this.incident, this.case_number);
+        } else {
+          // Unselect logic
+          this.onUnselect(this.address, this.case_number);
+        }
+  
+        // Toggle the selected state
+        this.selected = !this.selected;
+      },
+    },
+    computed: {
+      buttonText() {
+        return this.selected ? "Unselect" : "Select";
+      },
+    },
+  };
+  </script>
