855af4b121ee45746f228ab8f4d9b44cf58b3bdb
Jonny DeMarais <dema5892@stthomas.edu>
2023-12-19 13:16:21 -0600

    Adding files

---
+++ src/views/About.vue
@@ -0,0 +1,175 @@
+<template>
+  <section class="grid-container">
+    <div class="grid-x grid-margin-x">
+      <div class="cell small-12 text-center">
+        <h1 class="page-title">About the Project</h1>
+        <h2>The Developers</h2>
+        <!-- Add content about your project -->
+        <section class="developers-section">
+          <div class="developer">
+            <img src="../pictures/Jonny DeMarais.jpg" alt="Developer 1" class="developer-photo" />
+            <div class="developer-bio-box">
+              <p class="developer-bio">Jonny DeMarais is a senior at The University of St. Thomas studying 
+              Computer Science with a minor in Catholic Studies.</p>
+            </div>
+          </div>
+          <div class="developer">
+            <img src="../pictures/Noah Friesen.jpg" alt="Developer 2" class="developer-photo" />
+            <div class="developer-bio-box">
+              <p class="developer-bio">Short bio for Developer 3.</p>
+            </div>
+          </div>
+          <div class="developer">
+            <img src="../pictures/Catherine Keiffer.jpg" alt="Developer 3" class="developer-photo" />
+            <div class="developer-bio-box">
+              <p class="developer-bio">Short bio for Developer 3.</p>
+            </div>
+          </div>
+        </section>
+        <div class="section-spacing"></div>
+        <h2>Tools</h2>
+        <section class="tools-section">
+          <div class="tool">
+            <p>Vue.js</p>
+          </div>
+          <div class="tool">
+            <p>Leaflet</p>
+          </div>
+          <div class="tool">
+            <p>Nominatim</p>
+          </div>
+        </section>
+      </div>
+      
+      <div class="cell small-12 text-center">
+        <div class="section-spacing"></div>
+        <h2>Video Presentation</h2>
+        <div class="video-container">
+          <iframe width="560" height="315" :src="youtubeEmbedLink" frameborder="0" allowfullscreen></iframe>
+        </div>
+      </div>
+      <!-- Interesting Findings section -->
+      <div class="cell small-12 text-center">
+        <div class="section-spacing"></div>
+        <h2>Interesting Findings</h2>
+        <div class="interesting-findings">
+          <div class="finding">
+            <h3>First Finding</h3>
+            <p class="finding-description">Description of the first finding goes here.</p>
+          </div>
+          <div class="finding">
+            <h3>Second Finding</h3>
+            <p class="finding-description">Description of the second finding goes here.</p>
+          </div>
+          <div class="finding">
+            <h3>Third Finding</h3>
+            <p class="finding-description">Description of the third finding goes here.</p>
+          </div>
+          <div class="finding">
+            <h3>Fourth Finding</h3>
+            <p class="finding-description">Description of the second finding goes here.</p>
+          </div>
+          <div class="finding">
+            <h3>Fifth Finding</h3>
+            <p class="finding-description">Description of the second finding goes here.</p>
+          </div>
+          <div class="finding">
+            <h3>Sixth Finding</h3>
+            <p class="finding-description">Description of the second finding goes here.</p>
+          </div>
+        </div>
+        <!-- Add content for interesting findings -->
+      </div>
+    </div>
+ 
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      youtubeEmbedLink: "https://www.youtube.com/embed/YrxBCBibVo0"
+    };
+  }
+
+}
+
+</script>
+
+<style>
+
+.page-title {
+  font-size: 2.5rem;
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 20px;
+}
+.text-center {
+  text-align: center;
+}
+.developers-section {
+  display: flex;
+  justify-content: space-around;
+  margin-top: 40px;
+}
+.developer {
+  text-align: center;
+}
+.developer-photo {
+  width: 150px; /* Adjust the width of the photos */
+  height: 150px;
+  border-radius: 50%; /* Make the images circular */
+  margin-bottom: 15px; /* Add some space between the photo and bio */
+  object-fit: cover; /* Maintain aspect ratio without distortion */
+
+}
+.developer-bio-box {
+  background-color: #f5f5f5; /* Set background color for the bio box */
+  padding: 10px; /* Add padding around the bio */
+  border-radius: 5px; /* Add rounded corners to the bio box */
+  max-width: 250px; /* Set maximum width for the bio box */
+  margin: 0 auto; /* Center the box horizontally */
+}
+.developer-bio {
+  font-size: 1.2rem;
+  margin: 0; /* Remove default margins to fit content within the box */
+  text-align: justify; /* Align text to justify for better readability */
+  max-width: 100%; /* Limit text width within the box */
+}
+.tools-section {
+  display: flex;
+  justify-content: space-around;
+  margin-top: 40px;
+}
+.tool {
+  text-align: center;
+  background-color: #f5f5f5; /* Set background color for the tool box */
+  padding: 10px; /* Add padding around the tool */
+  border-radius: 5px; /* Add rounded corners to the tool box */
+  width: 200px; /* Adjust the width of the tool box */
+}
+.tool p {
+  margin: 0; /* Remove default margins */
+  font-size:xx-large;
+}
+.video-container {
+  position: relative;
+  padding-bottom: 56.25%; /* Aspect ratio for 16:9 */
+  padding-top: 30px;
+  height: 0;
+  overflow: hidden;
+}
+
+.video-container iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+.section-spacing {
+  margin-top: 200px;
+}
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,471 @@
+<script setup>
+import { reactive, ref, onMounted, initCustomFormatter } from 'vue'
+
+let crime_url = ref('');
+let table = reactive([]);
+let dialog_err = ref(false);
+let map = reactive(
+    {
+        leaflet: null,
+        center: {
+            lat: 44.955139,
+            lng: -93.102222,
+            address: ''
+        },
+        zoom: 12,
+        bounds: {
+            nw: {lat: 45.008206, lng: -93.217977},
+            se: {lat: 44.883658, lng: -92.993787}
+        },
+        neighborhood_markers: [
+            {location: [44.942068, -93.020521], marker: null},
+            {location: [44.977413, -93.025156], marker: null},
+            {location: [44.931244, -93.079578], marker: null},
+            {location: [44.956192, -93.060189], marker: null},
+            {location: [44.978883, -93.068163], marker: null},
+            {location: [44.975766, -93.113887], marker: null},
+            {location: [44.959639, -93.121271], marker: null},
+            {location: [44.947700, -93.128505], marker: null},
+            {location: [44.930276, -93.119911], marker: null},
+            {location: [44.982752, -93.147910], marker: null},
+            {location: [44.963631, -93.167548], marker: null},
+            {location: [44.973971, -93.197965], marker: null},
+            {location: [44.949043, -93.178261], marker: null},
+            {location: [44.934848, -93.176736], marker: null},
+            {location: [44.913106, -93.170779], marker: null},
+            {location: [44.937705, -93.136997], marker: null},
+            {location: [44.949203, -93.093739], marker: null}
+        ]
+    }
+);
+
+// Vue callback for once <template> HTML has been added to web page
+onMounted(() => {
+    // Create Leaflet map (set bounds and valied zoom levels)
+    map.leaflet = L.map('leafletmap').setView([map.center.lat, map.center.lng], map.zoom);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        minZoom: 11,
+        maxZoom: 18
+    }).addTo(map.leaflet);
+    map.leaflet.setMaxBounds([[44.883658, -93.217977], [45.008206, -92.993787]]);
+
+    // write the address in that's on the center of the map when initially start the app
+    var address = document.getElementById("address");
+    var fetchUrl = "https://nominatim.openstreetmap.org/reverse?format=json&lat="+map.center.lat+"&lon="+map.center.lng;
+    fetch(fetchUrl)
+    .then((response) => {
+        return response.json();
+    })
+    .then((json) => {
+        var location = json.display_name;
+        address.value = location;
+        map.center.address = location;
+    });
+
+    // Get boundaries for St. Paul neighborhoods
+    let district_boundary = new L.geoJson();
+    district_boundary.addTo(map.leaflet);
+    fetch('data/StPaulDistrictCouncil.geojson')
+    .then((response) => {
+        return response.json();
+    })
+    .then((result) => {
+        result.features.forEach((value) => {
+            district_boundary.addData(value);
+        });
+    })
+    .catch((error) => {
+        console.log('Error:', error);
+    });
+
+    map.leaflet.on('moveend', function () {
+        var center = map.leaflet.getCenter();
+        var latitude = document.getElementById("latitude")
+        var longitude = document.getElementById("longitude")
+        var address = document.getElementById("address");
+
+        latitude.value = center.lat; //update the boxes with the current center
+        longitude.value = center.lng;
+
+        var fetchUrl = "https://nominatim.openstreetmap.org/reverse?format=json&lat="+map.center.lat+"&lon="+map.center.lng;
+        fetch(fetchUrl)
+        .then((response) => {
+           return response.json();
+        })
+        .then((json) => {
+            var location = json.display_name;
+            map.center = center;
+            address.value = location;
+            map.center.address = location;
+        });
+
+    });
+});
+
+function replaceIncompleteAddress(address) {
+  const addressParts = address.split(' ');
+
+  let addressNumberIndex = -1;
+  for (let i = 0; i < addressParts.length; i++) {
+    if (addressParts[i].includes('X')) {
+      addressNumberIndex = i;
+      break;
+    }
+  }
+
+  if (addressNumberIndex !== -1) {
+    addressParts[addressNumberIndex] = addressParts[addressNumberIndex].replace(/X/g, '0');
+  }
+
+  const updatedAddress = addressParts.join(' ');
+
+  return updatedAddress;
+}
+
+
+
+// FUNCTIONS
+// Function called once user has entered REST API URL
+function initializeCrimes() {
+    // TODO: get code and neighborhood data
+    //       get initial 1000 crimes
+    console.log(crime_url.value);
+    let code_map = {}
+    let neighborhood_map = {}
+    let crimes_num_table = {}
+    fetch(crime_url.value+"/codes")
+    .then((response) => {
+        return response.json();
+    })
+    .then((json) => {
+
+        json.forEach((code_object) => {
+            code_map[code_object.code] = code_object.type
+        })
+    })
+    .catch((error) => {
+        console.log("error")
+        console.log(error)
+    });
+
+    fetch(crime_url.value+"/neighborhoods")
+    .then((response) => {
+        return response.json();
+    })
+    .then((json) => {
+
+        json.forEach((neigh_object) => {
+            neighborhood_map[neigh_object.id] = neigh_object.name
+            crimes_num_table[neigh_object.name] = 0
+        })
+
+        console.log(crimes_num_table)
+    })
+    .catch((error) => {
+        console.log("error")
+        console.log(error)
+    });
+
+
+    fetch(crime_url.value+"/incidents")
+    .then((response) => {
+        return response.json(); //we need to tell it how we want the result, which is in json
+    })
+    .then((json) => {
+
+        json.forEach((crime) => {
+            table.push({
+                case_number: crime.case_number,
+                incident_type: code_map[crime.code],
+                incident: crime.incident,
+                grid: crime.police_grid,
+                neighborhood: neighborhood_map[crime.neighborhood_number],
+                block: replaceIncompleteAddress(crime.block),
+                date: crime.date,
+                time: crime.time,
+            });
+        });
+
+        
+        let test = []
+
+        json.forEach((crime) => {
+            test.push(crime)
+            Object.keys(crimes_num_table).forEach((neigh) => {
+                if(neighborhood_map[crime.neighborhood_number] == neigh) {
+                    crimes_num_table[neigh]++ 
+                }
+            })
+        })
+        
+        map.neighborhood_markers.forEach((marker, index) => {
+            L.marker(marker.location).addTo(map.leaflet).bindPopup(Object.values(crimes_num_table)[index].toString() + " Crimes").openPopup()
+        }) 
+    })
+    .catch((error) => {
+        console.log("error")
+        console.log(error)
+    });
+
+    console.log(table)
+}
+
+// Function called when user presses 'OK' on dialog box
+function closeDialog() {
+    let dialog = document.getElementById('rest-dialog');
+    let url_input = document.getElementById('dialog-url');
+    if (crime_url.value !== '' && url_input.checkValidity()) {
+        dialog_err.value = false;
+        dialog.close();
+        initializeCrimes();
+        console.log('valid');
+    }
+    else {
+        dialog_err.value = true;
+    }
+}
+
+function deleteIncident(caseNumber) {
+    return fetch(`${crime_url.value}/remove-incident`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ case_number: caseNumber }),
+    })
+    .then((response) => {
+        if (response.ok) {
+            console.log(`Incident with case number ${caseNumber} deleted successfully.`);
+            return response.json();
+        } else {
+            console.error('Failed to delete incident.');
+            throw new Error('Failed to delete incident');
+        }
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+        throw error;
+    });
+}
+
+// This function moves the map to the location specified 
+function pressGo() {
+    // pull all inputs in from the boxes 
+    var address = document.getElementById("address").value;
+    var latitude = document.getElementById("latitude").value;
+    var longitude = document.getElementById("longitude").value;
+    if (address.trim() !== "") { // see if an address was entered
+        address = address.replaceAll(" ", '+'); // change spaces to pluses
+        // console.log(address)
+        var baseUrl = "https://nominatim.openstreetmap.org/search?q="
+        var fetchUrl = baseUrl+address+"&format=json&polygon=1&addressdetails=1";
+        // console.log(fetchUrl);
+
+        fetch(fetchUrl)
+        .then((response) => {
+            return response.json();
+        })
+        .then((json) => {
+            var newCenter = L.latLng(json[0].lat, json[0].lon);
+            map.leaflet.setView(newCenter, 14, { animate: true }); // move the map to the new center and zoom in
+        });
+    } else if (latitude.trim() !== "" && longitude.trim() !== "") {
+        console.log("both lat and long are entred");
+        var fetchUrl = "https://nominatim.openstreetmap.org/reverse?format=json&lat="+latitude+"&lon="+longitude;
+        
+        fetch(fetchUrl)
+        .then((response) => {
+            return response.json();
+        })
+        .then((json) => {
+            console.log(json);
+            var location = json.display_name;
+            console.log(location);
+            //should we update the address box with this display name?
+            map.leaflet.setView([latitude, longitude], 14, { animate: true }); // move the map to the new center and zoom in
+        });
+
+    }
+}
+const newIncident = ref({
+    case_number: '',
+    date: '',
+    time: '',
+    code: '',
+    incident: '',
+    police_grid: '',
+    neighborhood_number: '',
+    block: ''
+  // Add other necessary fields for the new incident
+});
+
+// Method to handle submitting a new incident
+async function addNewIncident() {
+  try {
+    const response = await fetch(`${crime_url.value}/new-incident`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newIncident.value),
+    });
+
+    if (response.ok) {
+      console.log('New incident added successfully!');
+      // Reset the form after successful submission
+      newIncident.value = {
+        case_number: '',
+        date: '',
+        time: '',
+        code: '',
+        incident: '',
+        police_grid: '',
+        neighborhood_number: '',
+        block: ''
+        // Reset other fields here
+      };
+    } else {
+      console.error('Failed to add new incident.');
+      // Handle error or display error message to the user
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    // Handle network or other errors
+  }
+}
+
+</script>
+
+<template>
+    <dialog id="rest-dialog" open>
+        <h1 class="dialog-header">St. Paul Crime REST API</h1>
+        <label class="dialog-label">URL: </label>
+        <input id="dialog-url" class="dialog-input" type="url" v-model="crime_url" placeholder="http://localhost:8000" />
+        <p class="dialog-error" v-if="dialog_err">Error: must enter valid URL</p>
+        <br/>
+        <button class="button" type="button" @click="closeDialog">OK</button>
+    </dialog>
+    <div class="grid-container ">
+        <div class="grid-x grid-padding-x">
+            <div id="leafletmap" class="cell auto"></div>
+        </div>
+    </div>
+
+    <div class="ui-row">
+        <label>Address: </label><input id="address" type="text">
+    </div>
+
+    <!-- should I add a new thing for lat/long? and then depending on what is filled in do that? what if the user types some in both? -->
+    <div class="ui-row">
+        <label>Latitude: </label><input id="latitude" type="text">
+    </div>
+    <div class="ui-row">
+        <label>Longitude: </label><input id="longitude" type="text">
+    </div>
+
+    <div class="ui-row">
+        <button class="button" type="button" @click="pressGo">Go</button>
+    </div>
+  <!-- New Incident Form -->
+  <div class="new-incident-form">
+    <h2>Add New Incident</h2>
+    <form @submit.prevent="addNewIncident">
+      <label for="caseNumber">Case Number:</label>
+      <input id="caseNumber" type="text" v-model="newIncident.case_number" required>
+
+      <label for="date">Date:</label>
+      <input id="date" type="date" v-model="newIncident.date" required>
+
+      <label for="time">Time:</label>
+      <input id="time" type="time" v-model="newIncident.time" required>
+
+      <label for="code">Code:</label>
+      <input id="code" type="text" v-model="newIncident.code" required>
+
+      <label for="incident">Incident:</label>
+      <input id="incident" type="text" v-model="newIncident.incident" required>
+
+      <label for="police_grid">Police Grid:</label>
+      <input id="police_grid" type="text" v-model="newIncident.police_grid" required>
+
+      <label for="neighborhood_number">Neighborhood Number:</label>
+      <input id="neighborhood_number" type="number" v-model="newIncident.neighborhood_number" required>
+
+      <label for="block">Block:</label>
+      <input id="block" type="text" v-model="newIncident.block" required>
+
+
+      <button class="button" type="submit">Submit</button>
+    </form>
+  </div>
+
+
+    <table v-if="table.length > 0">
+        <thead>
+            <tr>
+                <th>case_number</th>
+                <th>incident_type</th>
+                <th>incident</th>
+                <th>police_grid</th>
+                <th>neighborhood_name</th>
+                <th>block</th>
+                <th>date</th>
+                <th>time</th>
+                <th>delete incident</th>
+            </tr>
+        </thead>
+        <tbody>
+
+            <tr v-for="item in table">
+                <td>{{ item.case_number }}</td>
+                <td> {{ item.incident_type }}</td>
+                <td>{{ item.incident }}</td>
+                <td>{{ item.grid }}</td>
+                <td>{{ item.neighborhood }}</td>
+                <td>{{ item.block }}</td>
+                <td>{{ item.date }}</td>
+                <td>{{ item.time }}</td>
+                <td><button class="button" type="button" @click="deleteIncident(item.case_number)">Delete</button></td>
+            </tr>
+        </tbody>
+    </table>
+    
+    
+</template>
+
+<style>
+#rest-dialog {
+    width: 20rem;
+    margin-top: 1rem;
+    z-index: 1000;
+}
+
+#leafletmap {
+    height: 500px;
+}
+
+.dialog-header {
+    font-size: 1.2rem;
+    font-weight: bold;
+}
+
+.dialog-label {
+    font-size: 1rem;
+}
+
+.dialog-input {
+    font-size: 1rem;
+    width: 100%;
+}
+
+.dialog-error {
+    font-size: 1rem;
+    color: #D32323;
+}
+
+.ui-row {
+    display: inline-block;
+    margin-right: 15px; /* Adjust spacing between input boxes */
+}
+</style>
